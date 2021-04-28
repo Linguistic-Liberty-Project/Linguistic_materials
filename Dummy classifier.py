0b35d2149e50396030994ff5a4770c2c407d0533
LidiiaMelnyk95 <lidiia.melnyk@gmail.com>
2021-04-28 16:58:05 +0200

    -upd

--- Dummy classifier.py
+++ Dummy classifier.py
@@ -13,15 +13,14 @@ x_train = list(x_train_df['Label'])
 x_test_df = pd.read_csv('/Users/lidiiamelnyk/Documents/hatespeech_zn_ua.csv', sep=';',
                          encoding='utf-8-sig',
                          float_precision='round_trip', header = 0)
-x_test = list(x_test_df['Label'])
+x_test = x_test_df['Label'].astype(float)
 y_test_df = pd.read_csv('/Users/lidiiamelnyk/Documents/hatespeech_zn_ru.csv', sep=';',
                          encoding='utf-8-sig',
                          float_precision='round_trip', header = 0)
-y_test = list(y_test_df['Label'])
+y_test = y_test_df['Label'].astype(float)
 
 clf = SVC(kernel='linear', C=1).fit(x_train, y_train)
 print(clf.score(x_test, y_test))
 clf = DummyClassifier(strategy='most_frequent', random_state=0)
 clf.fit(x_train, y_train)
-DummyClassifier(random_state=0, strategy='most_frequent')
 clf.score(x_test, y_test)
---
+++ test fasttext ua.py
@@ -0,0 +1,60 @@
+import csv
+
+import fasttext
+
+def load_data(path):
+    reader = csv.reader(open(path, "rU"), delimiter=',')
+    path = path + '.updated'
+    writer = csv.writer(open(path, 'w'), delimiter='\t')
+    for row in reader:
+        row = [col.replace('\n', ' ') for col in row]
+        writer.writerow(row)
+    #writer.writerows(reader)
+    file = open(path, "r", encoding="utf-8")
+    data = file.readlines()
+    return [line.split(";") for line in data]
+
+
+def save_data(path, data):
+    with open(path, 'w', encoding="utf-8") as f:
+        f.write("\n".join(data))
+
+
+def train():
+    traning_parameters = {'input': 'fasttext.train', 'epoch': 50000, 'lr': 0.85, 'wordNgrams': 1, 'verbose': 2,
+                          'minCount': 1, 'loss': "ns",
+                          'lrUpdateRate': 100, 'thread': 1, 'ws': 5, 'dim': 100}
+    model = fasttext.train_supervised(**traning_parameters)
+    model.save_model("model.bin")
+    return model
+
+
+def test(model):
+    f1_score = lambda precision, recall: 2 * ((precision * recall) / (precision + recall))
+    nexamples, recall, precision = model.test('fasttext.test')
+    print(f'recall: {recall}')
+    print(f'precision: {precision}')
+    print(f'f1 score: {f1_score(precision, recall)}')
+    print(f'number of examples: {nexamples}')
+
+
+
+def transform(input_file,  output_file):
+    # load data
+    data = load_data(input_file)
+    # transform it into fasttext format __label__other have a nice day
+    data = [f"__label__{line[1].rstrip()}\t{line[0]}" for line in data]
+    # and save the data
+    save_data(output_file, data)
+
+
+if __name__ == "_main_":
+        # load the model
+        path = '/Users/lidiiamelnyk/Documents/annotation_test_ua.csv'
+        model = fasttext.load_model("model.bin")
+        test(model)
+
+    # train the model
+model = test('/Users/lidiiamelnyk/Documents/annotation_test_ua.csv')
+   # test(model)
+    #model.predict()
